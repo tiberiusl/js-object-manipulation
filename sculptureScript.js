8e2883a75a0b5c20f9539573e61fe84482dbf1e5
Tiberius Min-Hong Lai <tlai4@sfsu.edu>
2025-10-23 10:19:38 -0700

    Add files via upload

---
+++ sculptureScript.js
@@ -0,0 +1,23 @@
+const sculptureList = require('./data.js'); // import data.js
+
+// const element = sculptureList[0] // <---- comment out this line in your solution!
+
+/*
+// this following snippet is just to show how to iterate an object's keys
+// comment this out in your solution!
+for (const key in element){
+    console.log(`${key}: ${element[key].length}`)
+}
+*/
+
+let sculptureListLengths = [];
+sculptureList.forEach(sculpture => {
+    sculptureListLengths.push({
+        name: sculpture.name.length,
+        artist: sculpture.artist.length,
+        description: sculpture.description.length,
+        url: sculpture.url.length,
+        alt: sculpture.alt.length
+    })
+})
+console.log(sculptureListLengths);
